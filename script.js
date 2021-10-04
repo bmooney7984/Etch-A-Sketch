0cd274cf69b364b3f1f9b299da00e761efad7f8c
Brendan Mooney <bmooney7984@gmail.com>
2021-10-04 17:10:16 -0400

    Made box number adjustable

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function makeGrid() {
+function makeGrid(boxesPerSide) {
+  if (boxesPerSide > 100) {
+    boxesPerSide = 100;
+  }
   const gridBox = document.querySelector('div');
 
-  for (let i = 1; i <= 16; i++) {
+  clearGridBox();
+
+  for (let i = 1; i <= boxesPerSide; i++) {
     const container = document.createElement('div');
     container.classList.add('container');
     gridBox.append(container);
@@ -9,15 +14,13 @@ function makeGrid() {
 
   const containers = document.querySelectorAll('.container');
   containers.forEach(function(container) {
-    for (let i = 1; i <= 16; i++) {
+    for (let i = 1; i <= boxesPerSide; i++) {
       const newDiv = document.createElement('div');
       newDiv.classList.add('etchable');
       container.append(newDiv);
     }
   });
 }
-
-makeGrid();
 
 function makeEtchable(box) {
   box.addEventListener('mouseenter', function() {
@@ -32,4 +35,25 @@ function addTrail() {
   });
 }
 
+function addReset() {
+  button = document.querySelector('button');
+
+  button.addEventListener('click', function() {
+    const boxesPerSide = window.prompt('Number of squares per side (max 100):', '16');
+    makeGrid(boxesPerSide);
+    addTrail();
+  });
+}
+
+function clearGridBox() {
+  const containers = document.querySelectorAll('.gridBox div');
+  containers.forEach(function(container) {
+    container.remove();
+  })
+}
+
+makeGrid(16);
+
 addTrail();
+
+addReset();
